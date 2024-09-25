93c9291ddeb470ce2806f39bfd38deeb618c4502
Joelthorner <joeltorner@gmail.com>
2024-09-25 19:52:07 +0200

    Partial commit

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -1,1 +1,15 @@
-export const changelog = {};
+import { FilePlus } from "lucide-react";
+
+export const changelog = [
+  {
+    date: new Date('09/25/2024'),
+    items: [
+      {
+        icon: FilePlus,
+        title: 'Lorem ipsum dolor sit amet',
+        text: 'Lorem ipsum dolor sit amet text',
+        user: 'joelthorner',
+      },
+    ],
+  },
+];
