825b5209a4c01f31e40a66728b970a7507418d01
Joelthorner <joeltorner@gmail.com>
2024-09-27 17:57:17 +0200

    Effects partial commit

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,43 +1,88 @@
 import { Link } from "react-router-dom";
 import { Card as CardType } from "../data/cards";
 import { getStarClassName } from "../utils/getStarClassName";
+import { useRef } from "react";
+import { getCardStatusIcon } from "../utils/getCardStatusIcon";
 
 interface CardProps {
   card: CardType;
 }
 
 export default function Card({ card }: CardProps) {
-  let starClassName = "size-3 " + getStarClassName("rarity_" + card.rarity);
+  const cardRef = useRef<HTMLDivElement>(null);
 
-  let statusIcon = (
-    <>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="size-4 fill-emerald-500" viewBox="0 0 16 16">
-        <path
-          fillRule="evenodd"
-          d="M2 15.5V2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.74.439L8 13.069l-5.26 2.87A.5.5 0 0 1 2 15.5m8.854-9.646a.5.5 0 0 0-.708-.708L7.5 7.793 6.354 6.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"
-        />
-      </svg>
-    </>
-  );
+  const initialStyles: any = {
+    "--pointer-x": "50%",
+    "--pointer-y": "50%",
+    "--pointer-from-center": "0",
+    "--pointer-from-top": "0.5",
+    "--pointer-from-left": "0.5",
+    "--card-opacity": "0",
+    "--rotate-x": "0deg",
+    "--rotate-y": "0deg",
+    "--background-x": "50%",
+    "--background-y": "50%",
+    "--card-scale": "1",
+    "--translate-x": "0px",
+    "--translate-y": "0px",
+  };
 
-  if (card.status === "pending") {
-    statusIcon = (
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="size-4 fill-orange-500" viewBox="0 0 16 16">
-        <path d="M2 1.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1-.5-.5m2.5.5v1a3.5 3.5 0 0 0 1.989 3.158c.533.256 1.011.791 1.011 1.491v.702c0 .7-.478 1.235-1.011 1.491A3.5 3.5 0 0 0 4.5 13v1h7v-1a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351v-.702c0-.7.478-1.235 1.011-1.491A3.5 3.5 0 0 0 11.5 3V2z" />
-      </svg>
-    );
-  } else if (card.status === "falti") {
-    statusIcon = (
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="size-4 fill-rose-600 " viewBox="0 0 16 16">
-        <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
-        <path d="M8 4a.5.5 0 0 1 .5.5V6H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V7H6a.5.5 0 0 1 0-1h1.5V4.5A.5.5 0 0 1 8 4" />
-      </svg>
-    );
-  }
+  const finalStyles: any = {
+    "--pointer-x": "14.164%",
+    "--pointer-y": "8.499%",
+    "--pointer-from-center": "1",
+    "--pointer-from-top": "0.08499000000000001",
+    "--pointer-from-left": "0.14164",
+    "--card-opacity": "1",
+    "--rotate-x": "10.239deg",
+    "--rotate-y": "-20.75deg",
+    "--background-x": "40.683%",
+    "--background-y": "35.89%",
+    "--card-scale": "1",
+    "--translate-x": "0px",
+    "--translate-y": "0px",
+  };
+
+  // Funció per animar
+  const animateCSSVariables = function (duration: number = 3000) {
+    const startTime = performance.now();
+
+    function updateVariables(timestamp: number) {
+      const elapsedTime = timestamp - startTime;
+      const progress = Math.min(elapsedTime / duration, 1); // Assegura't que el valor està entre 0 i 1
+
+      // Actualitza les variables CSS de forma progressiva
+      for (const key in initialStyles) {
+        const initialValue = parseFloat(initialStyles[key]) || 0;
+        const finalValue = parseFloat(finalStyles[key]) || 0;
+
+        const unit = finalStyles[key].match(/[a-z%]+$/) || ""; // Detecta la unitat (%, px, deg)
+        const interpolatedValue = initialValue + (finalValue - initialValue) * progress;
+
+        if (cardRef.current) {
+          cardRef.current.style.setProperty(key, interpolatedValue + unit);
+        }
+      }
+
+      if (progress < 1) {
+        requestAnimationFrame(updateVariables);
+      }
+    }
+
+    requestAnimationFrame(updateVariables);
+  };
+
+  const starClassName = "size-3 " + getStarClassName("rarity_" + card.rarity);
+  const statusIcon = getCardStatusIcon(card.status);
 
   return (
-    <Link to={"/cards/" + card.id} className="h-full flex flex-col bg-white border shadow-sm rounded-xl p-2 hover:shadow-lg focus:outline-none focus:shadow-lg transition">
-      <div className="w-full aspect-[6/8]">
+    <Link
+      to={"/cards/" + card.id}
+      className="h-full flex flex-col bg-white border shadow-sm rounded-xl p-2 hover:shadow-lg focus:outline-none focus:shadow-lg transition"
+      onMouseEnter={() => animateCSSVariables(500)}
+    >
+      <div className="w-full aspect-[6/8] relative">
+        <div className="card-brilli-default absolute inset-0 z-20" ref={cardRef}></div>
         <img src={card.images[0]} alt={card.name} />
       </div>
 
@@ -45,14 +90,14 @@ export default function Card({ card }: CardProps) {
       <p className="mt-1 text-[10px] leading-tight font-medium uppercase text-gray-400 mb-2 line-clamp-2">{card.collection.name}</p>
 
       <div className="p-2 border-t -ml-2 -mr-2 -mb-2 mt-auto flex items-center gap-2 justify-between">
-        <div className="flex items-center gap-1 flex-1">
+        <div className="flex items-center gap-1">
           {statusIcon}
           <p className="text-[10px] text-gray-500 capitalize">{card.status}</p>
         </div>
-        <div className="flex items-center gap-1 flex-none justify-center">
-          <p className="text-[10px] text-gray-400 capitalize text-center">{card.year}</p>
+        <div className="flex items-center gap-1 justify-center ml-auto">
+          <p className="text-[10px] text-pink-600 capitalize text-center">{card.year}</p>
         </div>
-        <div className="flex items-center gap-1 flex-1 justify-end">
+        <div className="flex items-center gap-1 justify-end">
           <p className="text-[10px] text-gray-500 capitalize">{card.rarity}/5</p>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className={starClassName} viewBox="0 0 16 16">
             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
