f56bfb45154c9c062a4466c1a2131c8fcf8a7d3d
Joelthorner <joeltorner@gmail.com>
2024-11-21 18:58:04 +0100

    💋

--- src/data/cards/TOPPS_CHROME_FC_BARCELONA_2022_2023_57.ts
+++ src/data/cards/TOPPS_CHROME_FC_BARCELONA_2022_2023_57.ts
@@ -1,53 +1,53 @@
 import { Card, CardBrilli, CardType, CardStatus, CardProductType, CardOrientation } from "../../types/card";
 import { ToppsChromeFcBarcelona_2022_2023 } from "../collections";
 
-import aitanaBonmati_57ChromeFcb_22_23 from "../img/cards/aitana-bonmati-57-chrome-fcb-22-23.jpg";
-import aitanaBonmati_57ChromeFcb_22_23_refractor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_refractor.jpg";
-import aitanaBonmati_57ChromeFcb_22_23_bnw_refractor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_bnw_refractor.jpg";
-import aitanaBonmati_57ChromeFcb_22_23_bnw_refractor_back from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_bnw_refractor_back.webp";
-import aitanaBonmati_57ChromeFcb_22_23_x_factor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_x_factor.jpg";
-import aitanaBonmati_57ChromeFcb_22_23_x_factor_back from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_x_factor_back.webp";
-import aitanaBonmati_7ChromeFcb_22_23_fcb_refractor from "../img/cards/aitanaBonmati_7ChromeFcb_22_23_fcb_refractor.jpg";
-import aitanaBonmati_7ChromeFcb_22_23_fcb_refractor_back from "../img/cards/aitanaBonmati_7ChromeFcb_22_23_fcb_refractor_back.jpg";
-import aitanaBonmati_7ChromeFcb_22_23_blue_refractor from "../img/cards/aitanaBonmati_7ChromeFcb_22_23_blue_refractor.jpg";
-import aitanaBonmati_7ChromeFcb_22_23_blue_refractor_2 from "../img/cards/aitanaBonmati_7ChromeFcb_22_23_blue_refractor_2.webp";
-import aitanaBonmati_7ChromeFcb_22_23_blue_refractor_back from "../img/cards/aitanaBonmati_7ChromeFcb_22_23_blue_refractor_back.webp";
-import aitanaBonmati_57ChromeFcb_22_23_gold_refractor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_gold_refractor.jpg";
-import aitanaBonmati_57ChromeFcb_22_23_gold_refractor_back from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_gold_refractor_back.webp";
-import aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor.jpg";
-import aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor_2 from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor_2.webp";
-import aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor_back from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor_back.webp";
-import aitanaBonmati_57ChromeFcb_22_23_red_x_fractor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_red_x_fractor.jpg";
-import aitanaBonmati_57ChromeFcb_22_23_red_x_fractor_2 from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_red_x_fractor_2.webp";
-import aitanaBonmati_57ChromeFcb_22_23_red_x_fractor_back from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_red_x_fractor_back.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoN57 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoN57.jpg";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoN57_2 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoN57_2.jpg";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoN57_back from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoN57_back.jpg";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoBlueN57 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoBlueN57.jpg";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoBlueN57_2 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoBlueN57_2.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoBlueN57_back from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoBlueN57_back.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57.jpg";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_2 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_2.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_3 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_3.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_4 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_4.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_5 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_5.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_6 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_6.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_7 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_7.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldXN57 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldXN57.jpg";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldXN57_back from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldXN57_back.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoOrangeXN57 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoOrangeXN57.jpg";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoOrangeXN57_back from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoOrangeXN57_back.webp";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoPurpleN57 from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoPurpleN57.jpg";
-import toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoPurpleN57_back from "../img/cards/toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoPurpleN57_back.webp";
+import ab_57CFcb_22_23 from "../img/cards/aitana-bonmati-57-chrome-fcb-22-23.jpg";
+import ab_57CFcb_22_23_refractor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_refractor.jpg";
+import ab_57CFcb_22_23_bnw_refractor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_bnw_refractor.jpg";
+import ab_57CFcb_22_23_bnw_refractor_back from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_bnw_refractor_back.webp";
+import ab_57CFcb_22_23_x_factor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_x_factor.jpg";
+import ab_57CFcb_22_23_x_factor_back from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_x_factor_back.webp";
+import ab_7CFcb_22_23_fcb_refractor from "../img/cards/aitanaBonmati_7ChromeFcb_22_23_fcb_refractor.jpg";
+import ab_7CFcb_22_23_fcb_refractor_back from "../img/cards/aitanaBonmati_7ChromeFcb_22_23_fcb_refractor_back.jpg";
+import ab_7CFcb_22_23_blue_refractor from "../img/cards/aitanaBonmati_7ChromeFcb_22_23_blue_refractor.jpg";
+import ab_7CFcb_22_23_blue_refractor_2 from "../img/cards/aitanaBonmati_7ChromeFcb_22_23_blue_refractor_2.webp";
+import ab_7CFcb_22_23_blue_refractor_back from "../img/cards/aitanaBonmati_7ChromeFcb_22_23_blue_refractor_back.webp";
+import ab_57CFcb_22_23_gold_refractor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_gold_refractor.jpg";
+import ab_57CFcb_22_23_gold_refractor_back from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_gold_refractor_back.webp";
+import ab_57CFcb_22_23_orange_x_fractor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor.jpg";
+import ab_57CFcb_22_23_orange_x_fractor_2 from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor_2.webp";
+import ab_57CFcb_22_23_orange_x_fractor_back from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor_back.webp";
+import ab_57CFcb_22_23_red_x_fractor from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_red_x_fractor.jpg";
+import ab_57CFcb_22_23_red_x_fractor_2 from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_red_x_fractor_2.webp";
+import ab_57CFcb_22_23_red_x_fractor_back from "../img/cards/aitanaBonmati_57ChromeFcb_22_23_red_x_fractor_back.webp";
+import tcfcbmquc_2223_a_N57 from "../img/cards/tcfcbmquc_2223_a_N57.jpg";
+import tcfcbmquc_2223_a_N57_2 from "../img/cards/tcfcbmquc_2223_a_N57_2.jpg";
+import tcfcbmquc_2223_a_N57_back from "../img/cards/tcfcbmquc_2223_a_N57_back.jpg";
+import tcfcbmquc_2223_a_BlueN57 from "../img/cards/tcfcbmquc_2223_a_BlueN57.jpg";
+import tcfcbmquc_2223_a_BlueN57_2 from "../img/cards/tcfcbmquc_2223_a_BlueN57_2.webp";
+import tcfcbmquc_2223_a_BlueN57_back from "../img/cards/tcfcbmquc_2223_a_BlueN57_back.webp";
+import tcfcbmquc_2223_a_GoldN57 from "../img/cards/tcfcbmquc_2223_a_GoldN57.jpg";
+import tcfcbmquc_2223_a_GoldN57_2 from "../img/cards/tcfcbmquc_2223_a_GoldN57_2.webp";
+import tcfcbmquc_2223_a_GoldN57_3 from "../img/cards/tcfcbmquc_2223_a_GoldN57_3.webp";
+import tcfcbmquc_2223_a_GoldN57_4 from "../img/cards/tcfcbmquc_2223_a_GoldN57_4.webp";
+import tcfcbmquc_2223_a_GoldN57_5 from "../img/cards/tcfcbmquc_2223_a_GoldN57_5.webp";
+import tcfcbmquc_2223_a_GoldN57_6 from "../img/cards/tcfcbmquc_2223_a_GoldN57_6.webp";
+import tcfcbmquc_2223_a_GoldN57_7 from "../img/cards/tcfcbmquc_2223_a_GoldN57_7.webp";
+import tcfcbmquc_2223_a_GoldXN57 from "../img/cards/tcfcbmquc_2223_a_GoldXN57.jpg";
+import tcfcbmquc_2223_a_GoldXN57_back from "../img/cards/tcfcbmquc_2223_a_GoldXN57_back.webp";
+import tcfcbmquc_2223_a_OrangeXN57 from "../img/cards/tcfcbmquc_2223_a_OrangeXN57.jpg";
+import tcfcbmquc_2223_a_OrangeXN57_back from "../img/cards/tcfcbmquc_2223_a_OrangeXN57_back.webp";
+import tcfcbmquc_2223_a_PurpleN57 from "../img/cards/tcfcbmquc_2223_a_PurpleN57.jpg";
+import tcfcbmquc_2223_a_PurpleN57_back from "../img/cards/tcfcbmquc_2223_a_PurpleN57_back.webp";
 import a57rf_2 from "../img/cards/a57rf_2.webp";
 import a57rf_3 from "../img/cards/a57rf_3.webp";
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57: Card = {
-  customRelatedsRegexp: /(?<!AA)-7[a-z]*$/,
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-n57',
   number: 57,
   images: [
-    aitanaBonmati_57ChromeFcb_22_23,
+    ab_57CFcb_22_23,
   ],
   name: 'Aitana Bonmatí Base in action #57',
   rarity: 1,
@@ -64,10 +64,11 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57rf: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-57rf',
   number: '57rf',
   images: [
-    aitanaBonmati_57ChromeFcb_22_23_refractor,
+    ab_57CFcb_22_23_refractor,
     a57rf_2,
     a57rf_3,
   ],
@@ -88,11 +89,12 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57rf: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57x: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-57x',
   number: '57x',
   images: [
-    aitanaBonmati_57ChromeFcb_22_23_x_factor,
-    aitanaBonmati_57ChromeFcb_22_23_x_factor_back,
+    ab_57CFcb_22_23_x_factor,
+    ab_57CFcb_22_23_x_factor_back,
   ],
   name: 'Aitana Bonmatí Base in action / X-Fractor',
   rarity: 3,
@@ -111,11 +113,12 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57x: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57b: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-57b',
   number: '57b',
   images: [
-    aitanaBonmati_57ChromeFcb_22_23_bnw_refractor,
-    aitanaBonmati_57ChromeFcb_22_23_bnw_refractor_back,
+    ab_57CFcb_22_23_bnw_refractor,
+    ab_57CFcb_22_23_bnw_refractor_back,
   ],
   name: 'Aitana Bonmatí Base in action / Black & White Wave',
   rarity: 4,
@@ -135,11 +138,12 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57b: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57ba: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-57ba',
   number: '57ba',
   images: [
-    aitanaBonmati_7ChromeFcb_22_23_fcb_refractor,
-    aitanaBonmati_7ChromeFcb_22_23_fcb_refractor_back,
+    ab_7CFcb_22_23_fcb_refractor,
+    ab_7CFcb_22_23_fcb_refractor_back,
   ],
   name: 'Aitana Bonmatí Base in action / Barça',
   rarity: 3,
@@ -159,12 +163,13 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57ba: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57bu: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-57bu',
   number: '57bu',
   images: [
-    aitanaBonmati_7ChromeFcb_22_23_blue_refractor,
-    aitanaBonmati_7ChromeFcb_22_23_blue_refractor_2,
-    aitanaBonmati_7ChromeFcb_22_23_blue_refractor_back,
+    ab_7CFcb_22_23_blue_refractor,
+    ab_7CFcb_22_23_blue_refractor_2,
+    ab_7CFcb_22_23_blue_refractor_back,
   ],
   name: 'Aitana Bonmatí Base in action / Blue',
   rarity: 4,
@@ -184,11 +189,12 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57bu: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57g: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-57g',
   number: '57g',
   images: [
-    aitanaBonmati_57ChromeFcb_22_23_gold_refractor,
-    aitanaBonmati_57ChromeFcb_22_23_gold_refractor_back,
+    ab_57CFcb_22_23_gold_refractor,
+    ab_57CFcb_22_23_gold_refractor_back,
   ],
   name: 'Aitana Bonmatí Base in action / Gold',
   rarity: 5,
@@ -208,12 +214,13 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57g: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57o: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-57o',
   number: '57o',
   images: [
-    aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor,
-    aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor_2,
-    aitanaBonmati_57ChromeFcb_22_23_orange_x_fractor_back,
+    ab_57CFcb_22_23_orange_x_fractor,
+    ab_57CFcb_22_23_orange_x_fractor_2,
+    ab_57CFcb_22_23_orange_x_fractor_back,
   ],
   name: 'Aitana Bonmatí Base in action / Orange X-Fractor',
   rarity: 5,
@@ -234,12 +241,13 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57o: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57r: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-57r',
   number: '57r',
   images: [
-    aitanaBonmati_57ChromeFcb_22_23_red_x_fractor,
-    aitanaBonmati_57ChromeFcb_22_23_red_x_fractor_2,
-    aitanaBonmati_57ChromeFcb_22_23_red_x_fractor_back,
+    ab_57CFcb_22_23_red_x_fractor,
+    ab_57CFcb_22_23_red_x_fractor_2,
+    ab_57CFcb_22_23_red_x_fractor_back,
   ],
   name: 'Aitana Bonmatí Base in action / Red X-Fractor',
   rarity: 5,
@@ -260,12 +268,13 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_57r: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_AB: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-AU-AB',
   number: 'AU-AB',
   images: [
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoN57,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoN57_2,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoN57_back,
+    tcfcbmquc_2223_a_N57,
+    tcfcbmquc_2223_a_N57_2,
+    tcfcbmquc_2223_a_N57_back,
   ],
   name: 'Aitana Bonmatí Action Autograph',
   rarity: 3,
@@ -283,11 +292,12 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_AB: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_ABy: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-AU-ABy',
   number: 'AU-ABy',
   images: [
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldXN57,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldXN57_back,
+    tcfcbmquc_2223_a_GoldXN57,
+    tcfcbmquc_2223_a_GoldXN57_back,
   ],
   name: 'Aitana Bonmatí Action Autographs / Yellow X-Fractor',
   rarity: 4,
@@ -308,12 +318,13 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_ABy: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_ABb: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-AU-ABb',
   number: 'AU-ABb',
   images: [
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoBlueN57,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoBlueN57_2,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoBlueN57_back,
+    tcfcbmquc_2223_a_BlueN57,
+    tcfcbmquc_2223_a_BlueN57_2,
+    tcfcbmquc_2223_a_BlueN57_back,
   ],
   name: 'Aitana Bonmatí Action Autograph / Blue',
   rarity: 4,
@@ -333,16 +344,17 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_ABb: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_ABg: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-AU-ABg',
   number: 'AU-ABg',
   images: [
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_2,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_3,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_4,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_5,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_6,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoGoldN57_7,
+    tcfcbmquc_2223_a_GoldN57,
+    tcfcbmquc_2223_a_GoldN57_2,
+    tcfcbmquc_2223_a_GoldN57_3,
+    tcfcbmquc_2223_a_GoldN57_4,
+    tcfcbmquc_2223_a_GoldN57_5,
+    tcfcbmquc_2223_a_GoldN57_6,
+    tcfcbmquc_2223_a_GoldN57_7,
   ],
   name: 'Aitana Bonmatí Action Autograph / Gold',
   rarity: 4,
@@ -362,11 +374,12 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_ABg: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_ABo: Card = {
-  id: 'topps-chrome-fc-barcelona-2022-2023-auto-orange-AU-ABo',
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
+  id: 'topps-chrome-fc-barcelona-2022-2023-AU-ABo',
   number: 'AU-ABo',
   images: [
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoOrangeXN57,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoOrangeXN57_back,
+    tcfcbmquc_2223_a_OrangeXN57,
+    tcfcbmquc_2223_a_OrangeXN57_back,
   ],
   name: 'Aitana Bonmatí Action Autograph / Orange X-Fractor',
   rarity: 5,
@@ -387,11 +400,12 @@ export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_ABo: Card = {
 };
 
 export const TOPPS_CHROME_FC_BARCELONA_2022_2023_AU_ABpu: Card = {
+  customRelatedsRegexp: /(57[a-z]*|AU-AB[a-z]*)$/,
   id: 'topps-chrome-fc-barcelona-2022-2023-AU-ABpu',
   number: 'AU-ABpu',
   images: [
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoPurpleN57,
-    toppsChromeFcBarcelonaMésQueUnClub_2022_23AutoPurpleN57_back,
+    tcfcbmquc_2223_a_PurpleN57,
+    tcfcbmquc_2223_a_PurpleN57_back,
   ],
   name: 'Aitana Bonmatí Action Autograph / Purple',
   rarity: 5,
